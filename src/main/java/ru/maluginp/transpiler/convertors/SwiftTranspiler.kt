a6d4f8f6504b020d76faeb8548eea20565640336
Platon Malyugin <malugin.p@gmail.com>
2020-03-16 21:55:58 +0300

    Start Swift transpiler

--- src/main/java/ru/maluginp/transpiler/convertors/SwiftTranspiler.kt
+++ src/main/java/ru/maluginp/transpiler/convertors/SwiftTranspiler.kt
@@ -1,9 +1,10 @@
 package ru.maluginp.transpiler.convertors
 
 class SwiftTranspiler : Transpiler {
-    override fun format(element: TrElement): String {
-        return when(element) {
-            else -> "$element"
-        }
+    override fun format(el: TrElement): String {
+        return when(el) {
+            is TrInterface -> "protocol ${el.name}"
+            else -> "$el"
+        }+"\n"
     }
 }
